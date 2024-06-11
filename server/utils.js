472efc548b9af6fc5d74d6f08ccd25be80564120
SuCicada <pengyifu@gmail.com>
2024-06-12 00:06:46 +0900

    get_daily.js

--- server/utils.js
+++ server/utils.js
@@ -1,14 +1,17 @@
 const {format} = require("date-fns");
 const path = require("node:path");
 
-function getDailyFileName() {
-  const now = new Date();
-  const formattedDate = format(now, 'yyyy-MM-dd');
+function getDailyFileName(date) {
+  let formattedDate = date;
+  if (!date) {
+    const now = new Date();
+    formattedDate = format(now, 'yyyy-MM-dd');
+  }
   return `data/bible_pray_${formattedDate}.json`;
 }
 
-function getDailyS3KeyName() {
-  let file = getDailyFileName();
+function getDailyS3KeyName(date) {
+  let file = getDailyFileName(date);
   return path.join(process.env.AWS_S3_KEY_PREFIX, file);
 }
 
--- tool/get_daily.js
+++ tool/get_daily.js
@@ -2,7 +2,9 @@ const {getDailyS3KeyName} = require("../server/utils");
 const {getFileFromS3} = require("../server/s3");
 const dotenv = require('dotenv');
 dotenv.config();
-let key = getDailyS3KeyName();
+
+let date = process.argv[2];
+let key = getDailyS3KeyName(date);
 console.log(key);
 getFileFromS3(process.env.AWS_S3_BUCKET_NAME, key).then(data => {
   console.log(data);
